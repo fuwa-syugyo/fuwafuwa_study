df839214f3f3bf8ac6b8798dd0e9936bcc6ca922
fuwa <fuwafuwa41.glr@gmail.com>
2024-03-03 00:42:41 +0900

    3-1 done

---
+++ react_jissen/my-react/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import StateBasic from './StateBasic';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// setInterval(() => {
+//   root.render(
+//     <p>現在時刻：{(new Date()).toLocaleString()}</p>
+//   );
+// }, 1000);
+
+root.render(
+  <StateBasic init={0} />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
