450924905db4620d9fc1de7ba418ace1499a4004
fuwa <fuwafuwa41.glr@gmail.com>
2024-03-03 16:13:57 +0900

    3-2 done

--- react_jissen/my-react/src/index.js
+++ react_jissen/my-react/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StateBasic from './StateBasic';
+import books from './books'
+import ForNest from './ForNest';
+import ForFilter from './ForFilter'
+import ForSort from './ForSort'
+import SelectStyle from './SelectStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,9 +23,25 @@ root.render(
 //   );
 // }, 1000);
 
+// root.render(
+//   <StateBasic init={0} />
+// );
+
 root.render(
-  <StateBasic init={0} />
-);
+  <ForNest src={books} />
+)
+
+// root.render(
+//   <ForFilter src={books} />
+// )
+
+root.render(
+  <ForSort src={books} />
+)
+
+root.render(
+  <SelectStyle mode = "light"/>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
