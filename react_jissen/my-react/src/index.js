12d7e1a264673f2bf06779cc9fa17b5b23d87bc2
fuwa <fuwafuwa41.glr@gmail.com>
2024-03-03 17:36:05 +0900

    3-3 done

--- react_jissen/my-react/src/index.js
+++ react_jissen/my-react/src/index.js
@@ -9,6 +9,13 @@ import ForNest from './ForNest';
 import ForFilter from './ForFilter'
 import ForSort from './ForSort'
 import SelectStyle from './SelectStyle';
+import StyledPanel from './StyledPanel';
+import TitledPanel from './TitledPanel';
+import ListTemplate from './ListTemplate';
+import MyHello from './MyHello';
+import TypeProp from './TypeProp';
+import StateCounter from './StateCounter';
+import StateParent from './StateParent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -27,20 +34,67 @@ root.render(
 //   <StateBasic init={0} />
 // );
 
-root.render(
-  <ForNest src={books} />
-)
+// root.render(
+//   <ForNest src={books} />
+// )
 
 // root.render(
 //   <ForFilter src={books} />
 // )
 
-root.render(
-  <ForSort src={books} />
-)
+// root.render(
+//   <ForSort src={books} />
+// )
+
+// root.render(
+//   <SelectStyle mode = "light"/>
+// )
+
+// root.render(
+//   <StyledPanel>
+//     <p>メンバー募集中です</p>
+//     <p>ようこそ〜</p>
+//   </StyledPanel>
+// )
+
+// root.render(<TitledPanel>
+//   <p key="title">メンバーぼしゅうちゅう</p>
+//   <p key="body">ようこそです</p>
+// </TitledPanel>)
+
+// root.render(
+// <ListTemplate src={books}>
+//   {elem => (
+//     <>
+//       <dt>
+//         <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+//           {elem.title}（{elem.price}円）
+//         </a>
+//       </dt>
+//       <dd>{elem.summary}</dd>
+//     </>
+//   )}
+// </ListTemplate>)
+
+// root.render(
+//   <ListTemplate src={books} render={ elem => (
+//       <>
+//         <dt>
+//           <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+//             {elem.title}（{elem.price}円）
+//           </a>
+//         </dt>
+//         <dd>{elem.summary}</dd>
+//       </>
+//     )}>
+//   </ListTemplate>)
+
+// root.render(
+//   <MyHello />
+// )
 
 root.render(
-  <SelectStyle mode = "light"/>
+  <StateParent/>
 )
 
 // If you want to start measuring performance in your app, pass a function
